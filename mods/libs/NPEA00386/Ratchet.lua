3d8a04cd19c470e91f4e37b43e2e05bd21db13cc
robooooo <atune313@gmail.com>
2023-03-18 23:38:55 +0000

    Add scripting libs for GC

---
+++ mods/libs/NPEA00386/Ratchet.lua
@@ -0,0 +1,69 @@
+Ratchet = class('Ratchet')
+
+local mt = {}
+
+mt.__index = function(self, key) 
+	if self._addresses[key] ~= nil then
+		local obj = self._addresses[key]
+		
+		local mem = Ratchetron:ReadMemory(GAME_PID, obj.addr, obj.size)
+		
+		if obj._type == "float" then
+			return bytestofloat(mem)
+		end
+		
+		-- Fallback return int
+		return bytestoint(mem)
+	else
+		return Ratchet.__instanceDict[key]
+	end
+end
+
+mt.__newindex = function(self, key, value)
+	if self._addresses[key] ~= nil then
+		local obj = self._addresses[key]
+		
+		if obj._type == "float" then
+			Ratchetron:WriteMemory(GAME_PID, obj.addr, obj.size, floattobytes(value))
+		else
+			-- Default to int
+			Ratchetron:WriteMemory(GAME_PID, obj.addr, obj.size, inttobytes(value, obj.size))
+		end
+	else
+		rawset(self, key, value)
+	end
+end
+
+
+function Ratchet:initialize()
+	self._addresses = {
+		x = {
+			addr = 0x147F260,
+			size = 4,
+			_type = "float"
+		},
+		y = {
+			addr = 0x147F264,
+			size = 4,
+			_type = "float"
+		},
+		z = {
+			addr = 0x147F268,
+			size = 4,
+			_type = "float"
+		},
+		bolts = {
+			addr = 0x1329A90,
+			size = 4,
+			_type = "int"
+		},
+		state = {
+			addr = 0x01481474,
+			size = 4,
+			_type = "int"
+		}
+	}
+	setmetatable(self, mt)
+end
+
+ratchet = Ratchet()
