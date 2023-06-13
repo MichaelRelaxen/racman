1120390cf8eb64243fd056c05f7745c28cb0bd26
king_dedede <alexjbrokaw@gmail.com>
2023-06-12 19:35:39 -0700

    Added AutosplitterConfigForm

---
+++ RaCTrainer/AutosplitterConfigForm.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace racman
+{
+    partial class AutosplitterConfigForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutosplitterConfigForm));
+            this.routeSelectionListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.openFromFileButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.applyChangesButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.SourceColumn = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.DestColumn = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // routeSelectionListBox
+            // 
+            this.routeSelectionListBox.FormattingEnabled = true;
+            this.routeSelectionListBox.Location = new System.Drawing.Point(12, 25);
+            this.routeSelectionListBox.Name = "routeSelectionListBox";
+            this.routeSelectionListBox.Size = new System.Drawing.Size(121, 160);
+            this.routeSelectionListBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Split Route:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.applyChangesButton);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(139, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(291, 209);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Configuration";
+            // 
+            // addButton
+            // 
+            this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addButton.Location = new System.Drawing.Point(12, 188);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(30, 30);
+            this.addButton.TabIndex = 3;
+            this.addButton.Text = "+";
+            this.addButton.UseVisualStyleBackColor = true;
+            // 
+            // removeButton
+            // 
+            this.removeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.removeButton.Location = new System.Drawing.Point(48, 188);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(30, 30);
+            this.removeButton.TabIndex = 4;
+            this.removeButton.Text = "-";
+            this.removeButton.UseVisualStyleBackColor = true;
+            // 
+            // openFromFileButton
+            // 
+            this.openFromFileButton.Location = new System.Drawing.Point(84, 188);
+            this.openFromFileButton.Name = "openFromFileButton";
+            this.openFromFileButton.Size = new System.Drawing.Size(49, 30);
+            this.openFromFileButton.TabIndex = 5;
+            this.openFromFileButton.Text = "Open...";
+            this.openFromFileButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(47, 183);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(154, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name:";
+            // 
+            // applyChangesButton
+            // 
+            this.applyChangesButton.Location = new System.Drawing.Point(207, 182);
+            this.applyChangesButton.Name = "applyChangesButton";
+            this.applyChangesButton.Size = new System.Drawing.Size(77, 24);
+            this.applyChangesButton.TabIndex = 2;
+            this.applyChangesButton.Text = "Apply";
+            this.applyChangesButton.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.SourceColumn,
+            this.DestColumn});
+            this.dataGridView1.Location = new System.Drawing.Point(7, 16);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(276, 159);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // SourceColumn
+            // 
+            this.SourceColumn.HeaderText = "Source";
+            this.SourceColumn.Name = "SourceColumn";
+            this.SourceColumn.Width = 137;
+            // 
+            // DestColumn
+            // 
+            this.DestColumn.HeaderText = "Destination";
+            this.DestColumn.Name = "DestColumn";
+            this.DestColumn.Width = 136;
+            // 
+            // AutosplitterConfigForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(435, 221);
+            this.Controls.Add(this.openFromFileButton);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.routeSelectionListBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AutosplitterConfigForm";
+            this.Text = "Split Route Editor";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox routeSelectionListBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Button applyChangesButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button openFromFileButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewComboBoxColumn SourceColumn;
+        private System.Windows.Forms.DataGridViewComboBoxColumn DestColumn;
+    }
+}
