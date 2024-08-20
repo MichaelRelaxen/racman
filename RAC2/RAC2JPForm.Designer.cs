bb391a247ff8dcadb078db5061f1a4b1fffa23e7
AgentKlunk2 <62008031+AgentKlunk2@users.noreply.github.com>
2024-08-20 19:27:25 -0400

    RAC2 unlocks menu updated
    added all weapons, gadgets, and items to unlocks menu and added a separate check list for the three categories for ease of use

---
+++ RAC2/RAC2JPForm.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace racman
+{
+    partial class RAC2JPForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RAC2JPForm));
+            this.textBoxRari = new System.Windows.Forms.TextBox();
+            this.labelRari = new System.Windows.Forms.Label();
+            this.labelBolts = new System.Windows.Forms.Label();
+            this.textBoxBolts = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menusToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configureButtonCombosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.switchGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.inputDisplayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.patchLoaderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.memoryUtilitiesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonSlots = new System.Windows.Forms.Button();
+            this.AutosplitterCheckbox = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.loadPlanetButton = new System.Windows.Forms.Button();
+            this.planets_comboBox = new System.Windows.Forms.ComboBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxRari
+            // 
+            this.textBoxRari.Location = new System.Drawing.Point(12, 51);
+            this.textBoxRari.Name = "textBoxRari";
+            this.textBoxRari.Size = new System.Drawing.Size(100, 20);
+            this.textBoxRari.TabIndex = 0;
+            this.textBoxRari.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxRari_KeyDown);
+            // 
+            // labelRari
+            // 
+            this.labelRari.AutoSize = true;
+            this.labelRari.Location = new System.Drawing.Point(12, 35);
+            this.labelRari.Name = "labelRari";
+            this.labelRari.Size = new System.Drawing.Size(60, 13);
+            this.labelRari.TabIndex = 1;
+            this.labelRari.Text = "Raritanium:";
+            // 
+            // labelBolts
+            // 
+            this.labelBolts.AutoSize = true;
+            this.labelBolts.Location = new System.Drawing.Point(12, 74);
+            this.labelBolts.Name = "labelBolts";
+            this.labelBolts.Size = new System.Drawing.Size(33, 13);
+            this.labelBolts.TabIndex = 3;
+            this.labelBolts.Text = "Bolts:";
+            // 
+            // textBoxBolts
+            // 
+            this.textBoxBolts.Location = new System.Drawing.Point(12, 90);
+            this.textBoxBolts.Name = "textBoxBolts";
+            this.textBoxBolts.Size = new System.Drawing.Size(100, 20);
+            this.textBoxBolts.TabIndex = 2;
+            this.textBoxBolts.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxBolts_KeyDown);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menusToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(341, 24);
+            this.menuStrip1.TabIndex = 102;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menusToolStripMenuItem
+            // 
+            this.menusToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.configureButtonCombosToolStripMenuItem,
+            this.switchGameToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.inputDisplayToolStripMenuItem,
+            this.patchLoaderToolStripMenuItem,
+            this.memoryUtilitiesToolStripMenuItem});
+            this.menusToolStripMenuItem.Name = "menusToolStripMenuItem";
+            this.menusToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menusToolStripMenuItem.Text = "Menu";
+            this.menusToolStripMenuItem.Click += new System.EventHandler(this.menusToolStripMenuItem_Click);
+            // 
+            // configureButtonCombosToolStripMenuItem
+            // 
+            this.configureButtonCombosToolStripMenuItem.Enabled = false;
+            this.configureButtonCombosToolStripMenuItem.Name = "configureButtonCombosToolStripMenuItem";
+            this.configureButtonCombosToolStripMenuItem.Size = new System.Drawing.Size(212, 22);
+            this.configureButtonCombosToolStripMenuItem.Text = "Configure button combos";
+            // 
+            // switchGameToolStripMenuItem
+            // 
+            this.switchGameToolStripMenuItem.Name = "switchGameToolStripMenuItem";
+            this.switchGameToolStripMenuItem.Size = new System.Drawing.Size(212, 22);
+            this.switchGameToolStripMenuItem.Text = "Switch game";
+            this.switchGameToolStripMenuItem.Click += new System.EventHandler(this.switchGameToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(209, 6);
+            // 
+            // inputDisplayToolStripMenuItem
+            // 
+            this.inputDisplayToolStripMenuItem.Name = "inputDisplayToolStripMenuItem";
+            this.inputDisplayToolStripMenuItem.Size = new System.Drawing.Size(212, 22);
+            this.inputDisplayToolStripMenuItem.Text = "Input display";
+            this.inputDisplayToolStripMenuItem.Click += new System.EventHandler(this.inputDisplayToolStripMenuItem_Click);
+            // 
+            // patchLoaderToolStripMenuItem
+            // 
+            this.patchLoaderToolStripMenuItem.Name = "patchLoaderToolStripMenuItem";
+            this.patchLoaderToolStripMenuItem.Size = new System.Drawing.Size(212, 22);
+            this.patchLoaderToolStripMenuItem.Text = "Patch loader...";
+            this.patchLoaderToolStripMenuItem.Click += new System.EventHandler(this.patchLoaderToolStripMenuItem_Click);
+            // 
+            // memoryUtilitiesToolStripMenuItem
+            // 
+            this.memoryUtilitiesToolStripMenuItem.Name = "memoryUtilitiesToolStripMenuItem";
+            this.memoryUtilitiesToolStripMenuItem.Size = new System.Drawing.Size(212, 22);
+            this.memoryUtilitiesToolStripMenuItem.Text = "Memory utilities";
+            this.memoryUtilitiesToolStripMenuItem.Click += new System.EventHandler(this.memoryUtilitiesToolStripMenuItem_Click);
+            // 
+            // buttonSlots
+            // 
+            this.buttonSlots.Location = new System.Drawing.Point(136, 35);
+            this.buttonSlots.Name = "buttonSlots";
+            this.buttonSlots.Size = new System.Drawing.Size(193, 209);
+            this.buttonSlots.TabIndex = 103;
+            this.buttonSlots.Text = "SETUP SLOTS\r\n(no but for real)\r\n(i mean it this time)\r\n";
+            this.buttonSlots.UseVisualStyleBackColor = true;
+            this.buttonSlots.Click += new System.EventHandler(this.buttonSlots_Click);
+            // 
+            // AutosplitterCheckbox
+            // 
+            this.AutosplitterCheckbox.AutoSize = true;
+            this.AutosplitterCheckbox.Location = new System.Drawing.Point(12, 227);
+            this.AutosplitterCheckbox.Name = "AutosplitterCheckbox";
+            this.AutosplitterCheckbox.Size = new System.Drawing.Size(78, 17);
+            this.AutosplitterCheckbox.TabIndex = 105;
+            this.AutosplitterCheckbox.Text = "Autosplitter";
+            this.AutosplitterCheckbox.UseVisualStyleBackColor = true;
+            this.AutosplitterCheckbox.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(8, 184);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 23);
+            this.button2.TabIndex = 106;
+            this.button2.Text = "Input Display";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 113);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 13);
+            this.label6.TabIndex = 109;
+            this.label6.Text = "Load Planet:";
+            // 
+            // loadPlanetButton
+            // 
+            this.loadPlanetButton.Location = new System.Drawing.Point(12, 156);
+            this.loadPlanetButton.Name = "loadPlanetButton";
+            this.loadPlanetButton.Size = new System.Drawing.Size(97, 23);
+            this.loadPlanetButton.TabIndex = 107;
+            this.loadPlanetButton.Text = "Load";
+            this.loadPlanetButton.UseVisualStyleBackColor = true;
+            this.loadPlanetButton.Click += new System.EventHandler(this.loadPlanetButton_Click);
+            // 
+            // planets_comboBox
+            // 
+            this.planets_comboBox.FormattingEnabled = true;
+            this.planets_comboBox.Items.AddRange(new object[] {
+            "Aranos",
+            "Oozla",
+            "Maktar",
+            "Endako",
+            "Barlow",
+            "Feltzin",
+            "Notak",
+            "Siberius",
+            "Tabora",
+            "Dobbo",
+            "Hrugis",
+            "Joba",
+            "Todano",
+            "Boldan",
+            "Aranos 2",
+            "Gorn",
+            "Snivelak",
+            "Smolg",
+            "Damosel",
+            "Grelbin",
+            "Yeedil",
+            "Insomniac Museum",
+            "Dobbo Orbit",
+            "Damosel Orbit",
+            "Slim Cognito",
+            "Wupash",
+            "Jamming Array"});
+            this.planets_comboBox.Location = new System.Drawing.Point(12, 129);
+            this.planets_comboBox.Name = "planets_comboBox";
+            this.planets_comboBox.Size = new System.Drawing.Size(97, 21);
+            this.planets_comboBox.TabIndex = 108;
+            this.planets_comboBox.SelectedIndexChanged += new System.EventHandler(this.planets_comboBox_SelectedIndexChanged);
+            // 
+            // RAC2JPForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 256);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.loadPlanetButton);
+            this.Controls.Add(this.planets_comboBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.AutosplitterCheckbox);
+            this.Controls.Add(this.buttonSlots);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.labelBolts);
+            this.Controls.Add(this.textBoxBolts);
+            this.Controls.Add(this.labelRari);
+            this.Controls.Add(this.textBoxRari);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "RAC2JPForm";
+            this.Text = " Ratchet & Clank 2 - NPJA40002 (JP)";
+            this.Load += new System.EventHandler(this.RAC2JPForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxRari;
+        private System.Windows.Forms.Label labelRari;
+        private System.Windows.Forms.Label labelBolts;
+        private System.Windows.Forms.TextBox textBoxBolts;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menusToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem configureButtonCombosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem switchGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem patchLoaderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem memoryUtilitiesToolStripMenuItem;
+        private System.Windows.Forms.Button buttonSlots;
+        private System.Windows.Forms.CheckBox AutosplitterCheckbox;
+        private System.Windows.Forms.ToolStripMenuItem inputDisplayToolStripMenuItem;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button loadPlanetButton;
+        private System.Windows.Forms.ComboBox planets_comboBox;
+    }
+}
---
+++ RAC2/RC2Unlocks.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace racman
+{
+    public partial class RC2Unlocks : Form
+    {
+        public rac2 game;
+
+        private List<GCItem> weapons = new List<GCItem>()
+        {
+            //WEAPONS
+            new GCItem("Lancer", 0x1481A9E),
+            new GCItem("Gravity-Bomb", 0x1481AAA),
+            new GCItem("Chopper", 0x1481A96),
+            new GCItem("Seeker-Gun", 0x1481A98),
+            new GCItem("Pulse-Rifle", 0x1481A97),
+            new GCItem("Miniturret-Glove", 0x1481AA9),
+            new GCItem("Blitz-Gun", 0x1481A9A),
+            new GCItem("Shield-Charger", 0x1481AAD),
+            new GCItem("Synthenoid", 0x1481A9F),
+            new GCItem("Lava-Gun", 0x1481A9D),
+            new GCItem("Bouncer", 0x1481AA5),
+            new GCItem("Minirocket-Tube", 0x1481A9B),
+            new GCItem("Plasma-Coil", 0x1481A9C),
+            new GCItem("Hoverbomb-Gun", 0x1481A99),
+            new GCItem("Spiderbot-Glove", 0x1481AA0),
+            new GCItem("Sheepinator", 0x1481A90),
+            new GCItem("Tesla-Claw", 0x1481A92),
+            new GCItem("Bomb-Glove", 0x1481A8c),
+            new GCItem("Wolloper", 0x1481AB5),
+            new GCItem("Visi-bomb-Gun", 0x1481A8e),
+            new GCItem("Decoy Glove", 0x1481A91),
+            new GCItem("Zodiac", 0x1481AAB),
+            new GCItem("RYNO-II", 0x1481AAC),
+            new GCItem("Clank-Zapper", 0x1481A89),
+        };
+
+        private List<GCItem> gadgets = new List<GCItem>()
+        {
+            //GADGETS
+            new GCItem("Swingshot", 0x1481A8d),
+            new GCItem("Dynamo", 0x1481AA4),
+            new GCItem("Therminator", 0x1481AA7),
+            new GCItem("Tractor-Beam", 0x1481AAE),
+            new GCItem("Hypnomatic", 0x1481AB7),
+            new GCItem("Heli-Pack", 0x1481A82),
+            new GCItem("Thruster-Pack", 0x1481A83),
+            new GCItem("Gravity Boots", 0x1481A93),
+            new GCItem("Grindboots", 0x1481A94),
+            new GCItem("Charge Boots", 0x1481AB6),
+
+        };
+
+        private List<GCItem> items = new List<GCItem>()
+        {
+            
+            //ITEMS
+            new GCItem("Biker-Helmet", 0x1481AB0),
+            new GCItem("Glider", 0x1481A95),
+            new GCItem("Quark-Statuette(Magnetizer_pt1)", 0x1481AB1),
+            new GCItem("Armor-Magnetizer", 0x1481A87),
+            new GCItem("Box-Breaker", 0x1481AB2),
+            new GCItem("Mapper", 0x1481A85),
+            new GCItem("Electrolyzer", 0x1481AA6),
+            new GCItem("Infiltrator", 0x1481AB3),
+            new GCItem("HydroPack", 0x1481A84),
+            new GCItem("Levitator", 0x1481A88)
+        };
+
+        private List<uint> allItemAddresses = new List<uint>()
+        {
+            0x1481A82, 0x1481A83, 0x1481A84, 0x1481A85, 0x1481A88, 0x1481A89, 0x1481A8c, 0x1481A8d,
+            0x1481A8e, 0x1481A90, 0x1481A91, 0x1481A92, 0x1481A93, 0x1481A94, 0x1481A95, 0x1481A96,
+            0x1481A97, 0x1481A98, 0x1481A99, 0x1481A9A, 0x1481A9B, 0x1481A9C, 0x1481A9D, 0x1481A9E, 
+            0x1481A9F, 0x1481AA0, 0x1481AA4, 0x1481AA5, 0x1481AA6, 0x1481AA7, 0x1481AA9, 0x1481AAB, 
+            0x1481AAC, 0x1481AAD, 0x1481AAE, 0x1481AB0, 0x1481AB1, 0x1481AB2, 0x1481AB3, 0x1481AB5,
+            0x1481AB6, 0x1481AB7
+        };
+
+        public RC2Unlocks(rac2 game)
+        {
+            InitializeComponent();
+            this.game = game;
+        }
+
+        private void RC2Unlocks_Load(object sender, EventArgs e)
+        {
+            foreach (var it in weapons)
+            {
+                checkListWeapons.Items.Add(it.name, it.IsUnlocked(game));
+            }
+
+            foreach (var it in gadgets)
+            {
+                checkListGadgets.Items.Add(it.name, it.IsUnlocked(game));
+            }
+
+            foreach (var it in items)
+            {
+                checkListItems.Items.Add(it.name, it.IsUnlocked(game));
+            }
+
+        }
+
+        private void checkListWeapons_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            bool enable = true;
+            if (e.NewValue == CheckState.Checked) enable = true;
+            if (e.NewValue == CheckState.Unchecked) enable = false;
+            // WHY DOES THIS EXIST
+            if (e.NewValue == CheckState.Indeterminate) return;
+
+            var gameWeapon = weapons[e.Index];
+            gameWeapon.LockOrUnlock(game, enable);
+
+        }
+
+        private void checkListGadgets_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            bool enable = true;
+            if (e.NewValue == CheckState.Checked) enable = true;
+            if (e.NewValue == CheckState.Unchecked) enable = false;
+            if (e.NewValue == CheckState.Indeterminate) return;
+
+            var gameGadget = gadgets[e.Index];
+            gameGadget.LockOrUnlock(game, enable);
+
+        }
+        private void checkListItems_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            bool enable = true;
+            if (e.NewValue == CheckState.Checked) enable = true;
+            if (e.NewValue == CheckState.Unchecked) enable = false;
+            if (e.NewValue == CheckState.Indeterminate) return;
+
+            var gameItem = items[e.Index];
+            gameItem.LockOrUnlock(game, enable);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < checkListWeapons.Items.Count; i++) checkListWeapons.SetItemChecked(i, true);
+            foreach (var it in weapons) it.LockOrUnlock(game, true);
+
+            for (int i = 0; i < checkListGadgets.Items.Count; i++) checkListGadgets.SetItemChecked(i, true);
+            foreach (var it in gadgets) it.LockOrUnlock(game, true);
+
+            for (int i = 0; i < checkListItems.Items.Count; i++) checkListItems.SetItemChecked(i, true);
+            foreach (var it in items) it.LockOrUnlock(game, true);
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < checkListWeapons.Items.Count; i++) checkListWeapons.SetItemChecked(i, false);
+
+            for (int i = 0; i < checkListGadgets.Items.Count; i++) checkListGadgets.SetItemChecked(i, false);
+
+            for (int i = 0; i < checkListItems.Items.Count; i++) checkListItems.SetItemChecked(i, false);
+            foreach (var ita in allItemAddresses) new GCItem("???", ita).LockOrUnlock(game, false);
+        }
+
+        private void buttonUnlock_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < checkListWeapons.Items.Count; i++) checkListWeapons.SetItemChecked(i, true);
+
+            for (int i = 0; i < checkListGadgets.Items.Count; i++) checkListGadgets.SetItemChecked(i, true);
+
+            for (int i = 0; i < checkListItems.Items.Count; i++) checkListItems.SetItemChecked(i, true);
+            foreach (var ita in allItemAddresses) new GCItem("???", ita).LockOrUnlock(game, true);
+        }
+
+
+
+        private void checkListWeapons_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkListItems_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkListGadgets_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    // https://docs.google.com/spreadsheets/d/1rlQEmaPpJDOTZ4yImP3xkW5FbOTNGv8EwhIVreArLQs/
+    public class GCItem
+    {
+        public string name;
+        public uint unlock;
+
+        public GCItem(string name, uint unlock)
+        {
+            this.name = name;
+            this.unlock = unlock;
+        }
+
+        public void LockOrUnlock(rac2 game, bool enable) =>
+              game.api.WriteMemory(game.pid, unlock, new byte[] { Convert.ToByte(enable) });
+
+        public bool IsUnlocked(rac2 game)
+        {
+            var res = game.api.ReadMemory(game.pid, unlock, 1);
+            return res[0] == 1;
+        }
+    }
+}
