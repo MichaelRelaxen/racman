041189dac4a8102e7da13cf4548d082bd94c3272
MichaelRelaxen <bouvetproj@gmail.com>
2026-02-09 17:07:41 +0100

    add to isemulator clauze

--- RaCTrainer/RAC1/RAC1Form.cs
+++ RaCTrainer/RAC1/RAC1Form.cs
@@ -70,6 +70,7 @@ namespace racman
                 infHealth.Enabled = false;
                 FreezeAmmoCheckbox.Enabled = false;
                 FastLoadToggle.Enabled = false;
+                quartuGrindCheckbox.Enabled = false;
             }
         }
 
